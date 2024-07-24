6992ce4e6827ea9193664a73d7a20d71fa7cf4c3
Jeffin Scaria <jefinscaria123@gmail.com>
2024-07-24 16:26:36 +0530

    First Day

---
+++ script.js
@@ -0,0 +1,34 @@
+//const button = document.getElementById('eventbtn');
+//const text = document.getElementById('text')
+
+//button.addEventListener('click',function(){
+//  text.innerHTML='this is a changed text')
+//  alert ('clicked')
+//});
+
+// const persons ={
+//     firstName: "Kevin",
+//     lastName: "Doe",
+//     age:22,
+//     eyeColor: 'blue'
+// }
+
+// console.log(persons.firstName+""+"is"+""+persons.age+"old")
+//function square(num)
+//{
+   // return num*num;
+//}
+// let square=(num)=>{
+//     return num*num;
+// }
+
+// console.log(square(2));let square=(num)=>{
+//     return num*num;
+// }
+
+// console.log(square(2));
+let sum=(num1,num2)=>{
+    return num1+num2;
+}
+
+console.log(sum(2,3));
